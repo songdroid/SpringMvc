cb96efa5e172fa0e6d86ab64fab8abdcd4ecb17d
song <netsong7@naver.com>
2015-12-23 16:11:37 +0900

    회원 관리 기능 추가

--- src/main/java/com/netsong7/member/controller/RegisterController.java
+++ src/main/java/com/netsong7/member/controller/RegisterController.java
@@ -1,22 +1,44 @@
 package com.netsong7.member.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.netsong7.member.repository.RegisterRequest;
 
 @Controller
 public class RegisterController {
 	@RequestMapping("/register.member")
 	public String handleStep1(){
-		return "/member/register/step1.jsp";
+		return "/member/register/step1";
 	}
 	
 	@RequestMapping("/step2")
-	public String handleStep2(){
-		return "step2";
+	public String handleStep2(@RequestParam(value="agree", defaultValue="false") Boolean agree){
+		if(!agree){
+			return "/member/register/step1.jsp";
+		}
+		else{
+			return "step2";
+		}
 	}
 	
+	/*
 	@RequestMapping("/step3")
-	public String handleStep3(){
+	public ModelAndView handleStep3(RegisterRequest regReq){
+		// DB가입 처리
+		ModelAndView mav = new ModelAndView("step3");
+		mav.addObject("mem", regReq);
+		return mav;
+	}
+	*/
+	
+	@RequestMapping(value="/step3", method=RequestMethod.POST)
+	public String handleStep3(@ModelAttribute("mem") RegisterRequest regReq){
+		// DB가입 처리
 		return "step3";
 	}
 }
--- src/main/webapp/WEB-INF/views/member/register/step3.jsp
+++ src/main/webapp/WEB-INF/views/member/register/step3.jsp
@@ -1,9 +1,10 @@
-<%@ page contentType="text/html; charset=utf-8"%>
+<%@ page contentType="text/html; charset=utf-8" isELIgnored="false"%>
 <!DOCTYPE html>
 <html>
 <head><title>Insert title here</title></head>
 <body>
-	<p><strong>님</strong> 회원 가입을 축하합니다.</p>
-	<p><a href="/chap02">메인 페이지로 이동</a></p>
+	<p><strong>${mem.name}님</strong> 회원 가입을 축하합니다.</p>
+	<!-- <p><a href="/chap02/index.jsp">메인 페이지로 이동</a></p> -->
+	<p><a href="main">메인 페이지로 이동</a></p>
 </body>
 </html>
